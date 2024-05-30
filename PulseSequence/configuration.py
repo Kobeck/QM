a616fe7b3fc097edfcbf7ab2cf8e3c3baf5841be
Konstantin Beck <konstantin.beck@uni-hamburg.de>
2024-05-30 20:58:42 +0200

    Pulse sequence with gaussian pi and pi/2 pulses at 13GHz using octave. No Input to Kryo yet

--- PulseSequence/configuration.py
+++ PulseSequence/configuration.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from qm.octave import QmOctaveConfig 
+from set_octave import OctaveUnit, octave_declaration
 from qualang_tools.units import unit
 from qualang_tools.plot import interrupt_on_close
 from qualang_tools.results import progress_counter, fetching_tool
@@ -9,21 +10,44 @@ from qualang_tools.config.waveform_tools import *
 
 u = unit(coerce_to_integer=True)
 
+###########
+# Network #
+###########
+
+qop_ip = "192.168.1.106"
+cluster_name = "Cluster_RR_Lab"
+qop_port = None
+
+octave_port = 11241
+octave_1 = OctaveUnit("octave1", qop_ip, port=octave_port, con="con1")
+octaves = [octave_1]
+octave_config = octave_declaration(octaves)
+
+
 #############
 # Variables #
 #############
 
 #Frequencies
-SiV_freq = 13.6 * u.GHz #spin spin transition 
-SiV_IF_freq = 3.1 * u.MHz #RF Frequency for nuclear spins
-
+SiV_LO_freq = 13e9 #spin spin transition 
+LO = SiV_LO_freq
+SiV_IF_freq = 3.1e6 #RF Frequency for nuclear spins
 #pulse lenghts
-pulse_len = 300 * u.ns
+pulse_len = 1000
 initializazion_len = 3000 * u.ns
-readout_pulse_len = 1e6 * u.ns
+readout_pulse_len = 10000 * u.ns
+
 delay_len = 200 * u.ns #time between pi pulses
-pi_half_len = 15 * u.ns
-pi_len = 30 * u.ns 
+pi_half_len = 16 * u.ns
+pi_len = 32 * u.ns 
+
+rf_length = 1  # in ns
+half_flat_length = int( pi_half_len - 2 * rf_length)  # in ns
+flat_length = int( pi_len - 2 * rf_length)
+gauss_half_pulse_duration = int(2*rf_length + flat_length )
+# Flattop Gaussian
+flattop_half_gauss = flattop_gaussian_waveform(0.5, half_flat_length, rf_length)
+flattop_gauss = flattop_gaussian_waveform(0.5, flat_length, rf_length)
 ##########
 # LIMITS #
 ##########
@@ -31,30 +55,24 @@ pi_len = 30 * u.ns
 #max pulse length = 1 ms (for some reason... reference qua-libs NV scripts)
 #possible values in steps of 4 because 4ns = 1clock cycle
 
-##gaussian rise and fall
-rf_length = 10 # in ns
-flat_length = int( pulse_duration - 2 * rf_length)  
-gauss_pulse_duration = int(2*rf_length + flat_length )
-
-# Flattop Gaussian
-flattop_gauss = flattop_gaussian_waveform(0.5, flat_length, rf_length)  #tool to create samples
-
-####
-ocatve_config = QmOctaveConfig()
-ocatve_config.set_calibration_db(os.getcwd())
-ocatve_config.add_device_info("octave1", octave_ip, octave_port)
-
 trigger_delay = 87  # 57ns with QOP222 and above otherwise 87ns
 trigger_buffer = 15  # 18ns with QOP222 and above otherwise 15ns
+detection_delay = 87 
 
+signal_threshold = -2.000 #ADC Units
+
+
+octave = "octave1"
+con = "con1"
 config = {
     "version": 1,
     "controllers": {
-        "con1": {
+        con: {
             "analog_outputs": {
-                1: {"offset": 0.0, "delay": 0},
-                2: {"offset": 0.0, "delay": 0}, 
-                3: {"offset": 0.0, "delay": 0}, 
+                1: {"offset": 0.0},
+                2: {"offset": 0.0}, 
+                3: {"offset": 0.0}, 
+                4: {"offset": 0.0},
             },
             "digital_outputs": {
                 1: {},  # AOM/Laser
@@ -71,100 +89,136 @@ config = {
     },
     "elements": {
         "SiV": {
-            "RF_inputs": {"port": ("octave1", 1)},
+            "RF_inputs": {"port": (octave, 1)},
+            "RF_outputs": {"port": (octave, 1)},
             "intermediate_frequency": SiV_IF_freq,
             "operations": {
                 "cw": "constPulse",
-                "pi_half": "pi_half_pulse",
-                "pi": "pi_pulse",
+                "readout": "readoutPulse",
+                "pi": "piPulse",
+                "pi_half": "piHalfPulse",
             },
             "digitalInputs": {
-                "marker": {
+                "switch": {
                     "port": ("con1", 1),
-                "delay": trigger_delay,
-                "buffer": trigger_buffer,
+                    "delay": trigger_delay,
+                    "buffer": trigger_buffer,
                 },
             },
-        },
-        "SPCM": {
-            "singleInput": {"port": ("con1", 1)},
-            "digitalInputs": {  # for visualization in simulation
-                "marker": {
-                    "port": ("con1", 3),
-                    "delay": detection_delay,
-                    "buffer": 0,
-                },
-            },
-            "operations": {
-                "readout": "readout_pulse",
-            },
-            "outputs": {"out1": ("con1", 1)},
-            "outputPulseParameters": {
-                "signalThreshold": signal_threshold_1,  # ADC units
-                "signalPolarity": "Below",
-                "derivativeThreshold": -2_000,
-                "derivativePolarity": "Above",
-            }, 
-            "time_of_flight": detection_delay,
+            "time_of_flight": 24,
             "smearing": 0,
         },
+        # "SPCM": {
+        #     "singleInput": {"port": ("con1", 1)},
+        #     "digitalInputs": {  # for visualization in simulation
+        #         "switch": {
+        #             "port": ("con1", 1),
+        #             "delay": 87,
+        #             "buffer": 15,
+        #         },
+        #     },
+        #     "operations": {
+        #         "readout": "readoutPulse",
+        #     },
+        #     "outputs": {"out1": ("con1", 1)},
+        #     "outputPulseParameters": {
+        #         "signalThreshold": signal_threshold,  # ADC units
+        #         "signalPolarity": "Below",
+        #         "derivativeThreshold": -2_000,
+        #         "derivativePolarity": "Above",
+        #     }, 
+        #     "intermediate_frequency": SiV_IF_freq,
+        #     "time_of_flight": detection_delay,
+        #     "smearing": 0,
+        # },
         "signal": {
-            "singleInput": {"port": ("con1", 1)},  # not used
-            "outputs": {
-                "out1": ("con1", 1), # output channel 1
-                "out2": ("con1", 2)
-            }, 
-            "digitalInputs": {  # for visualization in simulation
-                "marker": {
-                    "port": ("con1", 3),
-                    "delay": 0,
-                    "buffer": 0,
-                },
+            "RF_inputs": {
+                "port": (octave, 2),
             },
-            "outputPulseParameters": {
-                "signalThreshold": -2_000,  # ADC units
-                "signalPolarity": "Below",
-                "derivativeThreshold": -2_000,
-                "derivativePolarity": "Above",
+            "RF_outputs": {
+                "out1": (octave, 2),
+            },
+            "digitalInputs": {  # for visualization in simulation
+                "switch": {
+                    "port": (con, 3),
+                    "delay": 87,
+                    "buffer": 15,
+                },
             },
             "time_of_flight":  24,
             "smearing": 0,
-            "intermediate_frequency": IF_freq,
+            "intermediate_frequency": SiV_IF_freq,
             "operations": {
-                "readout": "readout_pulse",
+                "readout": "readoutPulse",
             },
         },
     },
     "octaves": {
-        "octave1": {
+        octave: {
             "RF_outputs": {
                 1: {
-                    "LO_frequency": SiV_freq,
+                    "LO_frequency": LO,
                     "LO_source": "internal",  # can be external or internal. internal is the default
                     "output_mode": "always_on",  # can be: "always_on" / "always_off"/ "triggered" / "triggered_reversed". "always_off" is the default
                     "gain": 0,  # can be in the range [-20 : 0.5 : 20]dB
                 },
+                2: {
+                    "LO_frequency": LO,
+                    "LO_source": "internal",
+                    "output_mode": "always_on",
+                    "gain": 0,
+                },
             },
-            "connectivity": "con1",
-        },
+            "RF_inputs": {
+                1: {
+                    "LO_frequency": LO,
+                    "LO_source": "internal",  # internal is the default
+                    "IF_mode_I": "direct",  # can be: "direct" / "mixer" / "envelope" / "off". direct is default
+                    "IF_mode_Q": "direct",
+                },
+                2: {
+                    "LO_frequency": LO,
+                    "LO_source": "external",  # external is the default
+                    "IF_mode_I": "direct",
+                    "IF_mode_Q": "direct",
+                },
+            },
+            "connectivity": con,
+        }
     },
-
     "pulses": {
         "constPulse": {
             "operation": "control",
             "length": pulse_len,  # in ns
-            "waveforms": {"single": "const_wf"},
-        },
-        "gaussPulse": {
-            "operation": "control",
-            "length":  gauss_pulse_duration, # in ns
-            "waveforms": {"single": "gauss_wf"},
-        },
-        "readout_pulse": {
-            "operation": "measurement",
-            "length": readout_pulse_duration,
+            "waveforms": {
+                "I": "const_wf",
+                "Q": "zero_wf",
+            },
             "digital_marker": "ON",
-            "waveforms": {"single": "zero_wf"},
+        },
+        "piPulse": {
+            "operation": "control",
+            "length": pi_len,  # in ns
+            "waveforms": {
+                "I": "gauss_wf",
+                "Q": "zero_wf",
+            },
+            "digital_marker": "ON",
+        },
+        "piHalfPulse": {
+            "operation": "control",
+            "length": pi_half_len,  # in ns
+            "waveforms": {
+                "I": "gauss_half_wf",
+                "Q": "zero_wf",
+            },
+            "digital_marker": "ON",
+        },
+        "readoutPulse": {
+            "operation": "measurement",
+            "length": readout_pulse_len,
+            "digital_marker": "ON",
+            "waveforms": {"I": "readout_wf", "Q": "zero_wf",},
         },
     },
     "waveforms": {
@@ -175,10 +229,14 @@ config = {
         "const_wf": {
             "type": "constant", "sample": 0.2
         },
+        "gauss_half_wf": {
+            "type": "arbitrary",
+            "samples": flattop_half_gauss,
+        },
         "gauss_wf": {
             "type": "arbitrary",
             "samples": flattop_gauss,
-        },
+        },       
         "readout_wf": {
             "type": "constant",
             "sample": 0.2,
--- PulseSequence/pulse_sequence.py
+++ PulseSequence/pulse_sequence.py
@@ -5,126 +5,89 @@ Created: 17/01/2021
 Revised by Tomer Feld - Quantum Machines
 Revision date: 04/04/2022
 """
+import os
 import time
 from qm import QuantumMachinesManager
 from qm.qua import *
 from qm import SimulationConfig
+from qm.octave import *
+from qm.octave.octave_manager import ClockMode
 import numpy as np
 import matplotlib.pyplot as plt
 from qualang_tools import units as u
 from configuration import *
 from scipy.fft import fft, ifft, fftfreq
 
+con = "con1"
+octave = "octave1"
+octave_config = QmOctaveConfig() # creates an octave config object
+octave_config.add_device_info(octave, qop_ip, octave_port)
+opx_port = None
+
 qop_ip = "192.168.1.106"
 cluster_name = "Cluster_RR_Lab"
-qmm = QuantumMachinesManager(host=qop_ip, cluster_name=cluster_name)
+octave_port = 11241
 
+qmm = QuantumMachinesManager(host=qop_ip, port=opx_port, cluster_name=cluster_name, octave=octave_config, log_level="DEBUG")
+qm = qmm.open_qm(config)
 simulate = False
 
-
+qm.octave.set_rf_output_gain("SiV", -10)
 with program() as sequence:
+    adc_str = declare_stream(adc_trace=True)
+    measure("readout", "signal", adc_str)
+    print(type(adc_str))
+    amplitude = 0.5
+    
+    play("cw" * amp(0), "SiV", duration=delay_len)
 
-    N = 8 # numer of pi pulses
-
-    adc_st = declare_stream(adc_trace=True)
-
-    measure("readout", "signal", adc_st)
-    #wait(25) #25 clock cycles = 100 ns
-
-    play("const" * amp(0), "qe1", duration=1e5)#, duration=1e9 * u.ns)
-    #reset_phase("qe1")
-    #play("const", "qe1")
-    #play("const" * amp(1), "qe1", chirp=(rate, units))
-    play("gauss", "qe1")
-    play("const" * amp(0), "qe1", duration=1e5)
-    #play("const" * amp(0), "qe1")
-    #play("gauss" * amp(1), "qe1")
-    #play("const" * amp(0), "qe1")
-    #play("gauss" * amp(1), "qe1")
-    #play("const" * amp(0), "qe1")
-
+    reset_phase("SiV")
+    play("pi_half" * amp(amplitude), "SiV")
+    
+    # 8 pulses
+    for i in range(8):
+        play("cw"* amp(0), "SiV", duration=delay_len)
+        reset_phase("SiV")
+        play("pi" * amp(amplitude), "SiV",)
+    
+    play("cw"* amp(0), "SiV", duration=delay_len)
+    reset_phase("SiV")
+    play("pi_half" * amp(amplitude), "SiV")
     
     with stream_processing():
-        adc_st.input1().save("adc1")
-        adc_st.input2().save("adc2")
-        adc_st.input1().fft(output="abs").save("adc1_fft")
-        adc_st.input2().fft(output="abs").save("adc2_fft")
-    
+        adc_str.input1().save("adc1")
+        adc_str.input2().save("adc2")
+
 # The integration weights are, constant, of length 10*4*6 = 240
 if simulate:
-    job = qmm.simulate(config, prog, SimulationConfig(int(pulse_duration) // 4))  # duration in clock cycles, 4 ns
+    job = qmm.simulate(config, sequence, SimulationConfig(int(pulse_len) // 4))  # duration in clock cycles, 4 ns
     samples = job.get_simulated_samples()
     x = samples.con1.analog["1"]
-    NFFT = 2**10
-    Fs = 1e9
-    ax1 = plt.subplot(111)
-    Pxx, freqs, bins, im = plt.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=100, cmap=plt.cm.gist_heat)
-    
-    ax1.set_xticklabels((ax1.get_xticks() * 1e6).astype(int))
-    ax1.set_yticklabels((ax1.get_yticks() / 1e6).astype(int))
-    plt.xlabel("t [us]")
-    plt.ylabel("f [MHz]")
-    plt.show()
+    plt.plot(x)
 else:
     print('Executing program')
     qm = qmm.open_qm(config)
-    job = qm.execute(prog)
+    job = qm.execute(sequence)
+
     res = job.result_handles
     res.wait_for_all_values()
-
-    #plot results
-    adc1 = u.raw2volts(res.get("adc1").fetch_all())
-    adc2 = u.raw2volts(res.get("adc2").fetch_all())
-    
-    fft1 = res.get("adc1_fft").fetch_all()
-    fft2 = res.get("adc2_fft").fetch_all()
-    np.savetxt("C:/Users/admin/QM/adc1.csv", adc1, delimiter=",")
-    np.savetxt("C:/Users/admin/QM/adc2.csv", adc2, delimiter=",")
-    np.savetxt("C:/Users/admin/QM/fft1.csv", fft1, delimiter=",")
-    np.savetxt("C:/Users/admin/QM/fft2.csv", fft2, delimiter=",")
-
-    #figures
-    fig, ax1= plt.subplots()   
-    plt.xlabel("Time [ns]")
-    plt.title("Single run")
-    plt.ylabel("Signal amplitude [V]")
-    ax1.plot(adc1, label="Pickup")
-    
-    ax1.tick_params(axis='y', labelcolor='blue')
-    ax2 = ax1.twinx()
-    ax2.plot(adc2, label="Pulse", color='orange')
-    ax2.tick_params(axis='y', labelcolor='orange')
-    ax2.set_ybound(-0.5,0.5)
-    ax1.set_ybound(ax2.get_ybound())
-    fig.legend(["Pickup", "Pulse"])
-
-    plt.show()
-    #ax2.ylabel("Signal amplitude [V]")
-## FFT
-    N = len(adc2)
-    # sample spacing
-    T = 1e-9 #1 readout every 4 ns
-    x = np.linspace(0.0, N*T, N, endpoint=False)
-    y1 = adc1
-    y2 = adc2
-    yf1 = fft(y1)
-    yf2 = fft(y2)
-
-    xf = fftfreq(N, T)[:N//2]
-    plt.plot(xf, 2.0/N * np.abs(yf1[0:N//2]))
-    plt.plot(xf, 2.0/N * np.abs(yf2[0:N//2]))
-    plt.grid()
-    #fig2, ax3 = plt.subplots()
-    plt.title("FFT")
-    plt.xlabel("Frequency [Hz]")
-    plt.xlim(-1e3, 1e4)
-    #plt.ylim(0, 0.05)
-    plt.legend(["Pickup", "Pulse"])
-    #ax3.plot(fft2)
-    #ax4 = ax3.twinx()
-    #ax4.plot(fft2)
-    plt.show()
-
-    # time.sleep(10)
     job.halt()
+    # fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1 = plt.subplot()
+    # fig.suptitle("Inputs from down conversion 1")
+    adc_1 = u.raw2volts(res.get("adc1").fetch_all())
+    ax1.plot(adc_1, label="Input 1")
+    ax1.set_title("amp VS time Input 1")
+    ax1.set_xlabel("Time [ns]")
+    ax1.set_ylabel("Signal amplitude [V]")
+    plt.show()
+    # adc_2 = u.raw2volts(res.get("adc2").fetch_all())
+    # ax2.plot(adc_2, label="Input 2")
+    # ax2.set_title("amp VS time Input 2")
+    # ax2.set_xlabel("Time [ns]")
+    # ax2.set_ylabel("Signal amplitude [V]")
+    # plt.tight_layout()
+    # plt.show()
+    
+    
 
